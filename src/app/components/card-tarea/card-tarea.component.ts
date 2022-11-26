22c7c3d2506e3a107aa9024289f7a88462174235
Fernando Quintanilla <df.quintanillah@gmail.com>
2022-11-25 22:59:47 -0600

    lista lista

--- src/app/components/card-tarea/card-tarea.component.ts
+++ src/app/components/card-tarea/card-tarea.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-tarea',
   templateUrl: './card-tarea.component.html',
-  styleUrls: ['./card-tarea.component.scss']
+  styleUrls: ['./card-tarea.component.scss'],
 })
 export class CardTareaComponent {
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  tareaHecha() {
+    console.log('Tarea hecha');
+  }
+
+  borrarTarea() {
+    console.log('Borrar tarea');
+  }
 }
